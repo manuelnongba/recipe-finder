6919c8734aca7f496db16cddaa45b7412e939290
Emmanuel Anongba <eanongba19@gmail.com>
2022-10-09 01:07:15 +0000

    Create config.js

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '53813735-a770-4f1d-a4d8-628b5d2cc5ba';
+export const MODAL_CLOSE_SEC = 2.5;
